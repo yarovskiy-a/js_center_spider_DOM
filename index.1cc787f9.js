77765d4bd0df2914a77b33ac05901c28aeedc9db
Yarovskiy Andrii <yarovskiy@gmail.com>
2026-03-12 14:12:59 +0100

    make build

---
+++ index.1cc787f9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientWidth,l=t.clientHeight,i=e.clientWidth,n=e.clientHeight;e.style.left="".concat((c-i)/2,"px"),e.style.top="".concat((l-n)/2,"px");
+//# sourceMappingURL=index.1cc787f9.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(`.wall`);
+const xWall = wall.clientWidth;
+const yWall = wall.clientHeight;
+const xSpider = spider.clientWidth;
+const ySpider = spider.clientHeight;
+spider.style.left = `${(xWall - xSpider) / 2}px`;
+spider.style.top = `${(yWall - ySpider) / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
